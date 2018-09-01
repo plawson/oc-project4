5bc0ceb225ee6ada7dfef52347afa2d18f5c2c52
Philippe Lawson <philippe.lawson@lawson-it.com>
2018-09-01 11:22:06 +0200

    Change directory structure

--- shl/create_hdfs_dirs.sh
+++ shl/create_hdfs_dirs.sh
@@ -2,8 +2,8 @@
 
 hdfs dfs -mkdir /data/frwiki
 hdfs dfs -mkdir /data/frwiki/raw
-hdfs dfs -mkdir /data/frwiki/master
-hdfs dfs -mkdir /data/frwiki/master/full
-hdfs dfs -mkdir /data/frwiki/master/test
+hdfs dfs -mkdir /data/frwiki/frwiki-20180501/master
+hdfs dfs -mkdir /data/frwiki/frwiki-20180501/master/full
+hdfs dfs -mkdir /data/frwiki/frwiki-20180501/master/test
 
 hdfs dfs -ls -R /data/frwiki
