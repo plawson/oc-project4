cf9ec5f60cc8d0526aa2b61e95d89d1b1e742f72
Philippe Lawson <philippe.lawson@lawson-it.com>
2018-09-11 01:27:20 +0200

    DOne with avro serialization

--- datalake/process_pagelinks.py
+++ datalake/process_pagelinks.py
@@ -35,14 +35,33 @@ with open(src) as f:
             index += 1
             for record in records:
                 pagelink = record.split(",")
+                pl_from = int(pagelink[0])
+                pl_namespace = int(pagelink[1])
+                pl_from_namespace = int(pagelink[len(pagelink)-1])
+                pl_title = ''
+                start = len(pagelink) - 2 - 1
+                stop = len(pagelink) - 1
+                index = start
+                while index < stop:
+                    if index == start:
+                        pl_title = pagelink[index][1:]
+                    elif index == stop - 1:
+                        pl_title += "," + pagelink[index][:-1]
+                    else:
+                        pl_title += "," + pagelink[index]
+                    index += 1
+                pl_title = pl_title.replace('_', ' ')
+                pl_title = pl_title.replace('\\"', '')
+                pl_title = pl_title.replace("\\'", "'")
                 pagelinks.append({
-                    "pl_from": int(pagelink[0]),
-                    "pl_namespace": int(pagelink[1]),
-                    "pl_title": pagelink[2][1:-1],
-                    "pl_from_namespace": int(pagelink[3])
+                    "pl_from": pl_from,
+                    "pl_namespace": pl_namespace,
+                    "pl_title": pl_title,
+                    "pl_from_namespace": pl_from_namespace
                 })
 
             if index >= limit:
+
                 with hdfs_client.write('{}pagelinks.{}.avro'.format(dst, pagelinkfileno)) as avro_file:
                     fastavro.writer(avro_file, schema, pagelinks)
                 index = 0
